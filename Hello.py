d27143abdcf6bdceddc9012d0d59cb6ae6b792f9
ThomJarland <148473532+ThomJarland@users.noreply.github.com>
2023-10-24 18:49:09 +0000

    modified:   Hello.py

--- Hello.py
+++ Hello.py
@@ -14,38 +14,15 @@
 
 import streamlit as st
 from streamlit.logger import get_logger
+import pandas as pd
 
 LOGGER = get_logger(__name__)
 
+DATA_URL = ("https://github.com/ashjdrae/PI2_A5_Equipe_106_Diot_Siaci/blob/main/city.csv")
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+def load_data(nrows):
 
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
-
-
-if __name__ == "__main__":
-    run()
+    data = pd.read_csv(DATA_URL, nrows=nrows, delimiter=",")
+    return data
+   
+data = load_data(10)
